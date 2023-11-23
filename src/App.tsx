a3e50a21780bb3943f60f279506ab3a91a2f84c4
Mahdi Aryayi <mahdiaryayi@gmail.com>
2023-11-23 23:45:25 +0330

    feat: added boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col">
+      <header>سلام مالیات</header>
+      <main>ماشین حساب مالیات</main>
+    </div>
+  );
+}
+
+export default App;
