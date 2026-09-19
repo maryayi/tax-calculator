83c158b4e8dbf76e9f66ac4f17de12ed69561e15
Mahdi Aryayi <mahdiaryayi@gmail.com>
2026-09-20 00:15:15 +0330

    fix: update vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import packageJson from './package.json';
+import packageJson from './package.json' with { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig({
